d743020b1e62809a78517d5b03f7ff6973fe7114
totto2727 <kaihatu.totto2727@gmail.com>
2022-05-06 19:28:19 +0000

    開始部以外完成

--- slack-database/get-twitter-list-tweet-lambda/main.go
+++ slack-database/get-twitter-list-tweet-lambda/main.go
@@ -12,72 +12,116 @@ import (
 )
 
 var (
-	API             = "twitter"
 	TWEET_URL       = "https://twitter.com/%s/status/%s"
-	CHANNEL         = "la-priere"
 	DATETIME_FORMAT = time.RFC3339
-	MAX_RESULTS     = 20
 )
+
+type MyEvent struct {
+	TwitterList string `json:"twitter_list"`
+	UpdatedAt   string `json:"updated_at"`
+}
 
 type ResultItem struct {
 	ID        string `json:"id"`
-	AuthorID  string `json:"author_id"`
+	UserID    string `json:"author_id"`
 	CreatedAt string `json:"created_at"`
 	UserName  string `json:"user_name"`
 	URL       string `json:"url"`
 }
 
 type Result struct {
-	Channel     string       `json:"channel"`
 	Data        []ResultItem `json:"data"`
-	API         string       `json:"api"`
-	ResultCount int          `json:"result_count"`
-	UpdateedAt  string       `json:"updated_at"`
+	MessageCount int          `json:"message_count"`
+	UpdatedAt   string       `json:"updated_at"`
 }
 
-func handler() (Result, error) {
+func handler(ctx context.Context, event MyEvent) (Result, error) {
 	client := gotwtr.New(os.Getenv("BEARER"))
-	// log.Println(os.Getenv("BEARER"))
-
-	ts, err := client.LookUpListTweets(context.Background(), "1516932575876378625", &gotwtr.ListTweetsOption{
-		MaxResults:  MAX_RESULTS,
-		Expansions:  []gotwtr.Expansion{gotwtr.ExpansionAuthorID},
-		TweetFields: []gotwtr.TweetField{gotwtr.TweetFieldCreatedAt},
-	})
+	lastUpdatedAt, err := time.Parse(DATETIME_FORMAT, event.UpdatedAt)
 	if err != nil {
 		log.Panicln(err)
 	}
 
-	authorMap := map[string]*gotwtr.User{}
-	for _, u := range ts.Includes.Users {
-		authorMap[u.ID] = u
-	}
-
-	resultItems := []ResultItem{}
-	for _, t := range ts.Tweets {
-		author := authorMap[t.AuthorID]
-		userName := author.UserName
-		URL := fmt.Sprintf(TWEET_URL, userName, t.ID)
-		// 2022-05-04T03:41:21.000Z
-		createdAt, err := time.Parse(time.RFC3339, t.CreatedAt)
-		if err != nil {
-			log.Panicln(err)
+	getListTweets := func(paginationToken string) (*gotwtr.ListTweetsResponse, error) {
+		var paginationToken_ string
+		if len(paginationToken) != 0 {
+			paginationToken_ = paginationToken
 		}
-		log.Println(URL)
-		resultItems = append(resultItems, ResultItem{
-			UserName:  userName,
-			AuthorID:  t.AuthorID,
-			ID:        t.ID,
-			CreatedAt: createdAt.Format(DATETIME_FORMAT),
-			URL:       URL,
+		return client.LookUpListTweets(context.Background(), event.TwitterList, &gotwtr.ListTweetsOption{
+			PaginationToken: paginationToken_,
+			MaxResults:      20,
+			Expansions:      []gotwtr.Expansion{gotwtr.ExpansionAuthorID},
+			TweetFields:     []gotwtr.TweetField{gotwtr.TweetFieldCreatedAt},
 		})
 	}
 
+	getResultItems := func(listTweetsResponse *gotwtr.ListTweetsResponse) ([]ResultItem, bool) {
+		authorMap := map[string]*gotwtr.User{}
+		for _, u := range listTweetsResponse.Includes.Users {
+			authorMap[u.ID] = u
+		}
+
+		resultItems := []ResultItem{}
+		for _, t := range listTweetsResponse.Tweets {
+			author := authorMap[t.AuthorID]
+			userName := author.UserName
+			url := fmt.Sprintf(TWEET_URL, userName, t.ID)
+			// 2022-05-04T03:41:21.000Z
+			createdAt, err := time.Parse(time.RFC3339, t.CreatedAt)
+			if err != nil {
+				log.Panicln(err)
+			}
+
+			if !createdAt.After(lastUpdatedAt) {
+				return resultItems, true
+			}
+
+			createdAtFormatted := createdAt.Format(DATETIME_FORMAT)
+			resultItems = append(resultItems, ResultItem{
+				ID:        t.ID,
+				UserID:    t.AuthorID,
+				UserName:  author.UserName,
+				CreatedAt: createdAtFormatted,
+				URL:       url,
+			})
+			log.Printf("%s %s", createdAtFormatted, url)
+		}
+		return resultItems, false
+	}
+
+	listTweetsResponse, err := getListTweets("")
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	resultItems, isLast := getResultItems(listTweetsResponse)
+
+	if !isLast {
+		for {
+			log.Println("Next Page")
+			time.Sleep(1 * time.Second)
+			listTweetsResponse, err = getListTweets(listTweetsResponse.Meta.NextToken)
+			if err != nil {
+				log.Panicln(err)
+			}
+			nextResultItems, isLast := getResultItems(listTweetsResponse)
+			resultItems = append(resultItems, nextResultItems...)
+			if isLast {
+				break
+			}
+		}
+	}
+
+	for i, j := 0, len(resultItems)-1; i < j; i, j = i+1, j-1 {
+		resultItems[i], resultItems[j] = resultItems[j], resultItems[i]
+	}
+
+	updatedAt := resultItems[len(resultItems)-1].CreatedAt
+	tweetCount := len(resultItems)
+	log.Printf("%s %s %d", event.UpdatedAt, updatedAt, tweetCount)
 	return Result{
-		API:         API,
-		Channel:     CHANNEL,
-		ResultCount: ts.Meta.ResultCount,
-		UpdateedAt:  time.Now().UTC().Format(DATETIME_FORMAT),
+		MessageCount: tweetCount,
+		UpdatedAt:   updatedAt,
 		Data:        resultItems,
 	}, nil
 }
